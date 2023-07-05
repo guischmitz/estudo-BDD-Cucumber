4226ae6d089e98b8894e473371119af0918e53a4
guilherme.silva <guilherme.silva@orsegups.com.br>
2023-07-05 10:50:07 -0300

    integração do JUnit para rodar os cenários

---
+++ CursoCucumber/src/test/java/Runner.java
@@ -0,0 +1,7 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/aula06_cucumber.feature" )
+public class Runner { }
